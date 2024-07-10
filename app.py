7217f8f40b3579329b57b5437e54286d9fdc3d1c
Nihal Johann Thomas <nihaljohannthomas@gmail.com>
2024-07-10 22:39:26 +0530

    Initializing the model

--- app.py
+++ app.py
@@ -1,24 +1,23 @@
-from temp import API_KEY
 import streamlit as st
+import os
+from dotenv import load_dotenv
 import google.generativeai as palm
-from langdetect import detect,LangDetectException
+from langdetect import detect
 from googletrans import Translator
 
-palm.configure(api_key=API_KEY)
+load_dotenv()
+
+Api_key = os.getenv("API_KEY")
+
+palm.configure(api_key=Api_key)
 translator = Translator()
 
-def generate_questions(model_name, text):
-    response = palm.generate_text(
-        model=model_name,
-        prompt=f"Generate questions from the following text:\n\n{text}\n\nQuestions:",
-        max_output_tokens=150
-    )
+i=0
+model_list = palm.list_models()
+for model in model_list:
+    if i == 1:
+        model_name = model.name
+        break
+    i += 1
 
-    questions = response.result.strip() if response.result else "No questions generated."
-    return questions
-
-model_name = 'models/text-bison-001'
-text = input("Enter the text from which you want questions to be generated for: ")
-
-generated_questions = generate_questions(model_name, text)
-print(f"Generated Questions:\n{generated_questions}")
+print(model_name)
